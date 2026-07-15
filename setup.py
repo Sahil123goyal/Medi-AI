16855e95a0b5a9e90441afbe71a85f9b7980cade
Sahil Goyal <sahilgoyalking667@gmail.com>
2026-07-15 19:16:54 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Medical_chatbot",
+    version="0.1.0",
+    author="Sahil Goyal",
+    author_email="sahilgoyalking667@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
